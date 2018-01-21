996592b8b5e8f008393b616d2309fc6a7f288d8c
Nitish Kakkar <nitishkakkar>
2018-01-21 15:39:57 +0530

    Application Insights basic sample.

---
+++ ApplicationInsightDemo/AppInsightSDK/Logger.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.DataContracts;
+using Microsoft.ApplicationInsights.Extensibility;
+using System;
+
+namespace AppInsightSDK
+{
+    public static class Logger
+    {
+        static TelemetryClient telemetryClient = null;
+        static Logger()
+        {
+            TelemetryConfiguration.Active.InstrumentationKey = "Instrumentation Key";
+            telemetryClient = new TelemetryClient();
+        }
+
+        public static void TrackInfo(string message)
+        {
+            telemetryClient.TrackTrace(message, SeverityLevel.Information);
+            telemetryClient.Flush();
+        }
+        
+        public static void TrackWarning(string message)
+        {
+            telemetryClient.TrackTrace(message, SeverityLevel.Warning);
+            telemetryClient.Flush();
+        }
+
+        public static void TrackException(Exception exception)
+        {
+            telemetryClient.TrackException(exception);
+            telemetryClient.Flush();
+        }
+    }
+}
---
+++ ApplicationInsightDemo/ApplicationInsightDemo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationInsightDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                AppInsightSDK.Logger.TrackInfo("Track Info");
+
+                AppInsightSDK.Logger.TrackWarning("Track Warning");
+
+                AppInsightSDK.Logger.TrackException(new ArgumentNullException("Parameter Name"));
+            }
+        }
+    }
+}
